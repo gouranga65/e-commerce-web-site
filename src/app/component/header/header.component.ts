04bdba608adcb434253a252fa0421b9c8f768f7f
gouranga <gm49302@gmail.com>
2023-06-23 20:12:06 +0530

    when seller will login header will change automatically

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
+  menuType="default";
+  constructor(private _router: Router) {}
+  ngOnInit() {
+    this._router.events.subscribe((result: any) => {
+      if (result.url) {
+        if (localStorage.getItem('seller') && result.url.includes('seller')) {
+          console.log('in seller area');
+          this.menuType="seller"
+        } else {
+          console.log('out seller area');
+          this.menuType="default";
+        }
+      }
+    });
+  }
 }
